228be4a62710d38400025463f5aa091ee50355fe
evaluli <eva.lickert@haufe-lexware.com>
2015-10-12 11:01:53 +0200

    Klasse Adresse vervollständigt :)

--- src/Adresse.java
+++ src/Adresse.java
@@ -2,5 +2,40 @@
  * Created by Thomas on 12.10.2015.
  */
 public class Adresse {
-    ouasdAS
+    private String strasse;
+    private int hausnr;
+    private String postleitzahl;
+    private String ort;
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public int getHausnr() {
+        return hausnr;
+    }
+
+    public void setHausnr(int hausnr) {
+        this.hausnr = hausnr;
+    }
+
+    public String getPostleitzahl() {
+        return postleitzahl;
+    }
+
+    public void setPostleitzahl(String postleitzahl) {
+        this.postleitzahl = postleitzahl;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
 }
