e198a1fc2488fde5cbeeddd0d9190b9809ea3157
evaluli <eva.lickert@haufe-lexware.com>
2015-10-12 11:04:20 +0200

    Klasse Adresse vervollständigt :)

---
+++ src/Kontakt.java
@@ -0,0 +1,41 @@
+/**
+ * Created by LickertE on 12.10.2015.
+ */
+public class Kontakt {
+    private String name;
+    private String vorname;
+    private String telefonnr;
+    private Adresse adresse;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getTelefonnr() {
+        return telefonnr;
+    }
+
+    public void setTelefonnr(String telefonnr) {
+        this.telefonnr = telefonnr;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+}
