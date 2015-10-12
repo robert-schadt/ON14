eff820b77bf089941f36d5daefa397969ec75f3d
mirjamdorfer <mir.doerfer.14@dhbw-mosbach.de>
2015-10-12 11:21:59 +0200

    Auskommentierte Ideen zum Aufbau verschiedener Fenster

--- src/View.java
+++ src/View.java
@@ -3,25 +3,23 @@
  */
 
 public class View extends Empfaenger {
-}
 
-    /* //Der Nutzer kann sich hier einloggen oder neuanmelden. Nach der Auswahl erscheint ein neues Fenster
-    public void einloggen(){
+    //Der Nutzer kann sich hier einloggen oder neuanmelden. Nach der Auswahl erscheint ein neues Fenster
+    public void einloggen() {
         JFrame fenster = new JFrame("Willkommen");
         //Damit das Fenster nicht mehr oben links in der Ecke aufgeht, sondern zentral auf dem Bildschirm
         fenster.setSize(220, 400);
         fenster.setLocationRelativeTo(null);
 
-        JPanel contentPane = (JPanel)fenster.getContentPane();
-        contentPane.setLayout(new GridLayout(0,1));
+        JPanel contentPane = (JPanel) fenster.getContentPane();
+        contentPane.setLayout(new GridLayout(0, 1));
         contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
         contentPane.add(new JLabel("Falls Sie schon einmal bei uns reserviert haben, klicken Sie bitte auf 'Registrierter Nutzer'."));
         contentPane.add(new JLabel(" Andernfalls wählen Sie bitte 'Neuer Nutzer'."));
         contentPane.add(new JLabel(" "));
 
         JButton neu = new JButton("Registrieren");
-        neu.addActionListener(new ActionListener()
-        {
+        neu.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //nutzerAnlegen();
                 fenster.dispose();
@@ -29,9 +27,8 @@ public class View extends Empfaenger {
         });
         contentPane.add(neu);
 
-        JButton login=new JButton("Einloggen");
-        login.addActionListener(new ActionListener()
-        {
+        JButton login = new JButton("Einloggen");
+        login.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //einloggen();
                 fenster.dispose();
@@ -45,13 +42,13 @@ public class View extends Empfaenger {
 
     }
 
-        // Anlegen eines neuen Nutzers. User-ID wird aus dem Vornamen, Nachnamen und Nummer generiert
-        public void nutzerAnlegen(){
-        JFrame fenster = new JFrame("Bitte registrieren Sie sich");
+    // Anlegen eines neuen Nutzers. User-ID wird aus dem Vornamen, Nachnamen und Nummer generiert
+    public void nutzerAnlegen() {
+        JFrame fenster = new JFrame("Bitte legen Sie einen neuen Kontakt an");
         fenster.setSize(220, 400);
         fenster.setLocationRelativeTo(null);
 
-        JPanel contentPane = (JPanel)fenster.getContentPane();
+        JPanel contentPane = (JPanel) fenster.getContentPane();
         contentPane.setBorder(new EmptyBorder(12, 12, 12, 12));
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
         contentPane.add(new JLabel("Vorname"));
@@ -66,43 +63,51 @@ public class View extends Empfaenger {
         JTextField tel = new JTextField("");
         contentPane.add(tel);
 
-        JLabel id = new JLabel("IHRE ID: ");
-        contentPane.add(id);
+        contentPane.add(new JLabel("Straße"));
+        JTextField tel = new JTextField("");
+        contentPane.add(stra);
 
-        JLabel pw = new JLabel("IHR PASSWORT: ");
-        contentPane.add(pw);
+        contentPane.add(new JLabel("Hausnummer"));
+        JTextField tel = new JTextField("");
+        contentPane.add(Hnr);
 
-        JButton reg = new JButton("Registrieren");
-        reg.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e) {
-                    registriert = true;
-                    id.setText("IHRE ID: "+ besucherID);
-                    pw.setText("IHR PASSWORT: beispielPasswort");
-                    pw.setForeground(Color.blue);
-                    id.setForeground(Color.blue);
+        contentPane.add(new JLabel("PLZ"));
+        JTextField tel = new JTextField("");
+        contentPane.add(plz;
 
-                }
-            });
+        contentPane.add(new JLabel("Ort"));
+        JTextField tel = new JTextField("");
+        contentPane.add(ort);
+
+
+        JButton reg = new JButton("Speichern");
+        reg.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                gespeichert = true;
+                id.setText("Der Kontakt wurde angelegt:  " + vorn + " " + nachn);
+                pw.setForeground(Color.blue);
+                id.setForeground(Color.blue);
+
+            }
+        });
         contentPane.add(reg);
 
         contentPane.add(new JLabel(" "));
         contentPane.add(new JLabel("Bitte merken Sie sich Ihre ID und Ihr Passwort für den nächsten Login."));
 
         JButton weiter = new JButton("Weiter");
-        weiter.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e) {
-                    if(registriert==true){
-                        Reservierungssystem();
-                        fenster.dispose();}
-                    else{
-                        nutzerAnlegen();
-                        fenster.dispose();
-                    }
-
+        weiter.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (registriert == true) {
+                    Reservierungssystem();
+                    fenster.dispose();
+                } else {
+                    nutzerAnlegen();
+                    fenster.dispose();
                 }
-            });
+
+            }
+        });
 
         contentPane.add(weiter);
 
@@ -113,12 +118,12 @@ public class View extends Empfaenger {
     }
 
     //Einloggen: Der Nutzer gibt seine Zugangsdaten ein. Das Passwort wird geschwärzt.
-    public void einloggen(){
+    public void einloggen() {
         JFrame fenster = new JFrame("Bitte loggen Sie sich ein");
         fenster.setSize(220, 400);
         fenster.setLocationRelativeTo(null);
 
-        JPanel contentPane = (JPanel)fenster.getContentPane();
+        JPanel contentPane = (JPanel) fenster.getContentPane();
         contentPane.setBorder(new EmptyBorder(12, 12, 12, 12));
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
         contentPane.add(new JLabel("Bitte geben Sie ihre ID als 'User' und das Passwort ein."));
@@ -135,24 +140,22 @@ public class View extends Empfaenger {
         JButton einloggen = new JButton("Einloggen");
         einloggen.addActionListener(new ActionListener()
 
-            {
-                public void actionPerformed(ActionEvent e) {
+        {
+            public void actionPerformed(ActionEvent e) {
 
-                    passwort = system.passwortUeberpruefen(new String(pw.getPassword()),user.getText());
+                passwort = system.passwortUeberpruefen(new String(pw.getPassword()), user.getText());
 
-                    if(passwort == true)
-                    {
-                        fenster.dispose();
-                        Reservierungssystem();
+                if (passwort == true) {
+                    fenster.dispose();
+                    Reservierungssystem();
 
-                    }
-                    else{
-                        fenster.dispose();
-                        einloggen();
+                } else {
+                    fenster.dispose();
+                    einloggen();
 
-                    }
                 }
-            });
+            }
+        });
         contentPane.add(einloggen);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -163,25 +166,24 @@ public class View extends Empfaenger {
 
 
     // Rückmeldung für erfolgreiche Aktionen
-    public void bestaetigung(){
+    public void bestaetigung() {
         JFrame fenster = new JFrame("Die Reservierung war erfolgreich");
         fenster.setSize(220, 400);
         fenster.setLocationRelativeTo(null);
 
-        JPanel contentPane = (JPanel)fenster.getContentPane();
+        JPanel contentPane = (JPanel) fenster.getContentPane();
         contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
         contentPane.setLayout(new BorderLayout());
 
         contentPane.add(new Label("Die Aktion verlief erfolgreich."), BorderLayout.NORTH);
         JButton ok = new JButton("Ok");
-        ok.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e) {
+        ok.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 
-                    fenster.dispose();
-                }
-            });
-        contentPane.add(ok,BorderLayout.SOUTH);
+                fenster.dispose();
+            }
+        });
+        contentPane.add(ok, BorderLayout.SOUTH);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         fenster.pack();
@@ -189,34 +191,33 @@ public class View extends Empfaenger {
     }
 
 
-        // Meldung für nicht erfolgreiche Aktionen
-        public void warnung(String s){
+    // Meldung für nicht erfolgreiche Aktionen
+    public void warnung(String s) {
 
         String fehler = s;
         JDialog fenster = new JDialog();
         fenster.setTitle("Warnung!");
-         fenster.setModal(true);
+        fenster.setModal(true);
 
         fenster.setSize(220, 400);
         fenster.setLocationRelativeTo(null);
 
-        JPanel contentPane = (JPanel)fenster.getContentPane();
+        JPanel contentPane = (JPanel) fenster.getContentPane();
         contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
         contentPane.setLayout(new BorderLayout());
 
-        contentPane.add(new Label(fehler),BorderLayout.NORTH);
+        contentPane.add(new Label(fehler), BorderLayout.NORTH);
         JButton ok = new JButton("Ok");
-        ok.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e) {
+        ok.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 
-                    fenster.dispose();
-                }
-            });
-        contentPane.add(ok,BorderLayout.SOUTH);
+                fenster.dispose();
+            }
+        });
+        contentPane.add(ok, BorderLayout.SOUTH);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         fenster.pack();
         fenster.setVisible(true);
     }
-*/
+}
